c1cd91569dcd13b2a5500f8562a5e4fd371f1d50
Sam <mrsan123450@gmail.com>
2026-05-24 12:44:32 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,18 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-
-  // Remove static export for now
-  // output: 'export',   ← Commented out
-
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      net: false,
-      tls: false,
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
